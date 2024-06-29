efe856b74faf3282bc6289e7f9ea7b7957fafc3a
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-06-29 18:12:51 +0100

    removed whitespace

--- src/main/java/com/craig/w/paymenttracker/model/services/HolidayService.java
+++ src/main/java/com/craig/w/paymenttracker/model/services/HolidayService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class    HolidayService {
+public class HolidayService {
 
     @Autowired
     private HolidayRepository holidayRepository;
