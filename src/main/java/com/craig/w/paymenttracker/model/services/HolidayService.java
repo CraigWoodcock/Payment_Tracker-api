1d92291325a57cfcfaca5c417a8b176367466473
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-07-02 15:15:59 +0100

    working index, need to fix people not showing on holiday details

--- src/main/java/com/craig/w/paymenttracker/model/services/HolidayService.java
+++ src/main/java/com/craig/w/paymenttracker/model/services/HolidayService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -46,7 +47,7 @@ public class HolidayService {
         for(String personName : names) {
             Person person = new Person();
             person.setName(personName.trim());
-            person.setToPay(totalAmount.divide(new BigDecimal(numPeople)));
+            person.setToPay(totalAmount.divide(new BigDecimal(numPeople), RoundingMode.HALF_UP));
             person.setHoliday(holiday);
             people.add(person);
         }
